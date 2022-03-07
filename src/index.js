807c73bc3bbd7d9231fb94adcc37f95f222708f4
narimanAbyar <nariman.abyar11@gmail.com>
2022-03-07 15:06:33 +0330

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
+function Numberlist(props){
+  const numbers=props.numbers;
+  const listItems=numbers.map((number)=> <li>{number}</li>)
+  return(
+   <ul>{listItems}</ul>
+  )
+
+  
+
+}
+
+const numbers=[1,2,3,4];
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <Numberlist numbers={numbers}/>
+ , document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
